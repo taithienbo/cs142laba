d875f8bcf010a5cc474a8f199b554b3da02dbcf1
Tai Bo <taithienbo@gmail.com>
2012-11-03 00:27:17 -0700

    prepared to work on lab5

--- src/crux/Compiler.java
+++ src/crux/Compiler.java
@@ -1,13 +1,13 @@
 package crux;
-
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.io.PrintStream;
 
 public class Compiler {
-	public static String studentName = "Tai Bo";
-	public static String studentID = "53907660";
-	public static String uciNetID = "tbo";
+	public static String studentName = "TODO: Your Name";
+	public static String studentID = "TODO: Your 8-digit id";
+	public static String uciNetID = "TODO: uci-net id";
 
 	public static void main(String[] args)
 	{
@@ -17,24 +17,41 @@ public class Compiler {
 		try 
 		{
 			s = new Scanner(new FileReader(sourceFilename));
-			Parser p;
-			p = new Parser(s);
-			p.parse();
-			if (p.hasError()) 
-			{
-				System.out.println("Error parsing file.");
-				System.out.println(p.errorReport());
-				System.exit(-3);
-			}
-			System.out.println("Crux program successfully parsed.");
-		} 
-		catch (IOException e) {
+		}
+		catch (IOException e) 
+		{
 			e.printStackTrace();
 			System.err.println("Error accessing the source file: \"" + sourceFilename + "\"");
 			System.exit(-2);
 		}
 
+		Parser p;
+		try {
+			p = new Parser(s);
+			ast.Command syntaxTree = p.parse();
 
+			if (p.hasError())
+			{
+				System.out.println("Error parsing file.");
+				System.out.println(p.errorReport());
+				System.exit(-3);
+			}
+
+			types.TypeChecker tc = new types.TypeChecker();
+			tc.check(syntaxTree);
+			if (tc.hasError()) 
+			{
+				System.out.println("Error type-checking file.");
+				System.out.println(tc.errorReport());
+				System.exit(-4);
+			}
+			System.out.println("Crux Program is has no type errors.");
+		} 
+		catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 }
--- src/crux/Symbol.java
+++ src/crux/Symbol.java
@@ -1,11 +1,17 @@
 package crux;
 
+import types.Type;
+import types.ErrorType;
+
 public class Symbol {
     
     private String name;
+    private Type type;
 
-    public Symbol(String name) {
+    public Symbol(String name) 
+    {
         this.name = name;
+        this.type = new ErrorType("Type not set.");
     }
     
     public String name()
@@ -13,9 +19,19 @@ public class Symbol {
         return this.name;
     }
     
+    public void setType(Type type)
+    {
+        this.type = type;
+    }
+    
+    public Type type()
+    {
+        return type;
+    }
+
     public String toString()
     {
-        return "Symbol(" + name + ")";
+        return "Symbol(" + name + ":" + type + ")";
     }
 
     public static Symbol newError(String message) 
